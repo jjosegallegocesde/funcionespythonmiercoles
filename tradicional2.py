03dac1946217f84d8a2f27d88127dfdae3f1675b
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-17 20:21:23 -0500

    nt9 feat se crea ejemplo de lambda

---
+++ tradicional2.py
@@ -0,0 +1,6 @@
+#software de nomina
+#gestion de vacaciones
+
+#sumar la cantidad de dias de vacaciones + dias libres ganados
+def sumarVaciones(diasVacaciones,diasLibres):
+    return diasVacaciones+diasLibres
